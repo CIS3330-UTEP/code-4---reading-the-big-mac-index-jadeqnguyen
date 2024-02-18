f33f75c4ee2a4a5a2efe03437d7f9fc444644464
jadeqnguyen <jqnguyen@miners.utep.edu>
2024-02-17 20:06:25 -0700

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -4,29 +4,42 @@ df = pd.read_csv("big-mac-full-index.csv")
 
 def get_big_mac_price_by_year(year,country_code):
     #filter data first
-    year = "date"
-    country_code = "iso_a3"
+    filt_df = df.loc[(df["year"] == year) & (df["iso_a3"] == country_code)]
+    mean_price = filt_df["dollar_price"].mean()
+    two_decimals = round(mean_price, 2)
     
-    query_text = f"iso_a3 == @county_code"
-    df_country_code = df.query(query_text)
-
-    print(round(df["dollar_price"].mean(),2))
+    return two_decimals
 
 def get_big_mac_price_by_country(country_code):
-    print(round(df["dollar.price"].mean(),2))
+    filt_df = df.loc[df["iso_a3"] == country_code]
+    
+    return round(df["dollar_price"].mean(),2)
+    
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    print(df["dollar_price"].min())
+    filt_df = df.loc[df["year"] == year]
+    min_price_country = df.loc[filt_df["dollar_price"].idxmin()]
+    
+    country_name = min_price_country["name"]
+    country_code = min_price_country["iso_a3"]
+    minimum_price = min_price_country["dollar_price"]
+    
+    return 
 
 def get_the_most_expensive_big_mac_price_by_year(year):
-    print(df["dollar.price"].max())
+    filt_df = df.loc[df["year"] == year]
+    max_price_country = df.loc[filt_df["dollar_price"].idxmax()]
+
+    country_name = max_price_country["name"]
+    country_code = max_price_country["iso_a3"]
+
 
 if __name__ == "__main__":
-    result_a = get_big_mac_price_by_year(2010,"arg")
-    print(result_a)
-    result_b = get_big_mac_price_by_country("mex")
-    print(result_b)
-    result_c = get_the_cheapest_big_mac_price_by_year(2008)
-    print()
-    result_d = get_the_most_expensive_big_mac_price_by_year(2014)
-    print(result_d)
+    # result_a = get_big_mac_price_by_year(2010,"arg")
+    # print(result_a)
+    # result_b = get_big_mac_price_by_country("mex")
+    # print(result_b)
+    # result_c = get_the_cheapest_big_mac_price_by_year(2008)
+    # print()
+    # result_d = get_the_most_expensive_big_mac_price_by_year(2014)
+    # print(result_d)
