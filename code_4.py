687e2d3a3935604a3ce0b2cb7b673bda250b0d88
jadeqnguyen <jqnguyen@miners.utep.edu>
2024-02-23 20:47:47 -0700

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -1,44 +1,42 @@
 import pandas as pd
+
 filename = "big-mac-full-index.csv"
 df = pd.read_csv(filename)
 
 def get_big_mac_price_by_year(year,country_code):
-    #filter data first
+    #filter data first 
     year = 2000
-    query_text = f"date > '{year}-01-01' and date < '{year}-12-31'"
+    query_text = f"(date >= '{year}-01-01' and (date <= '{year}-12-31') and (iso_a3 == '{country_code})"
     df_year = df.query(query_text)
+    
     return round(df_year["dollar_price"].mean(),2)
     
 def get_big_mac_price_by_country(country_code):
     country_code = "iso_a3"
     query_text = f"iso_a3 == {country_code}"
-    return round(df["dollar_price"].mean(),2)
+    df_country_code = df.query(query_text)
     
-
-# def get_the_cheapest_big_mac_price_by_year(year):
-#     filt_df = df.loc[df["year"] == year]
-#     min_price_country = filt_df.loc[filt_df["dollar_price"].idxmin()]
+    return round(df_country_code["dollar_price"].mean(),2)
     
-#     country_name = min_price_country["name"]
-#     country_code = min_price_country["iso_a3"]
-#     minimum_price = min_price_country["dollar_price"]
+def get_the_cheapest_big_mac_price_by_year(year):
+    query_text = f"(date == '{year}')"
+    df_cheapest_year = df.query(query_text)
     
-#     return f"{country_name}({country_code}): ${round(minimum_price, 2)}"
+    return df_cheapest_year['dollar_price'].idxmax()
+    
+def get_the_most_expensive_big_mac_price_by_year(year):
+    query_text = f"(date == '{year}')"
+    df_expensive_year = df.query(query_text)
+    
+    return df_expensive_year['dollar_price'].idxmin()
 
-# def get_the_most_expensive_big_mac_price_by_year(year):
-#     filt_df = df.loc[df["year"] == year]
-#     max_price_country = df.loc[filt_df["dollar_price"].idxmax()]
 
-#     country_name = max_price_country["name"]
-#     country_code = max_price_country["iso_a3"]
-
-
-# if __name__ == "__main__":
-#     result_a = get_big_mac_price_by_year(2010,"arg")
-#     print(result_a)
-    # result_b = get_big_mac_price_by_country("mex")
-    # print(result_b)
-    # result_c = get_the_cheapest_big_mac_price_by_year(2008)
-    # print()
-    # result_d = get_the_most_expensive_big_mac_price_by_year(2014)
-    # print(result_d)
+if __name__ == "__main__":
+    result_a = get_big_mac_price_by_year(2010,"arg")
+    print(result_a)
+    result_b = get_big_mac_price_by_country("mex")
+    print(result_b)
+    result_c = get_the_cheapest_big_mac_price_by_year(2008)
+    print()
+    result_d = get_the_most_expensive_big_mac_price_by_year(2014)
+    print(result_d)
