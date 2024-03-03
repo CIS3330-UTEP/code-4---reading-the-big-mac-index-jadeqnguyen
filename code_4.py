84ace599037f3dc514f858fcc3a73bb992e3d6bb
jadeqnguyen <jqnguyen@miners.utep.edu>
2024-03-02 22:44:45 -0700

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -5,30 +5,35 @@ df = pd.read_csv(filename)
 
 def get_big_mac_price_by_year(year,country_code):
     #filter data first 
-    query = f"(iso_a3 == '{country_code}' and ('{year}-01-01' < date < '{year}-12-31'))"
-    price_by_year = df.query(query)
+    country_code = country_code.upper()
+    query_text = f"(iso_a3 == '{country_code}' and ('{year}-01-01' < date < '{year}-12-31'))"
+    price_by_year = df.query(query_text)
+    mean_price_by_year = round(price_by_year['dollar_price'].mean(),2)
     
-    return round(price_by_year["dollar_price"].mean(),2)
+    return mean_price_by_year
     
 def get_big_mac_price_by_country(country_code):
-    query = f"(iso_a3 == '{country_code}')"
-    price_by_country = df.query(query)
+    country_code = country_code.upper()
+    query_text = f"(iso_a3 == '{country_code}')"
+    price_by_country = df.query(query_text)
+    mean_price_by_country = round(price_by_country["dollar_price"].mean(), 2)
     
-    return round(price_by_country["dollar_price"].mean(),2)
+    return mean_price_by_country
     
 def get_the_cheapest_big_mac_price_by_year(year):
     query = f"date == '{year}'"
-    df_cheapest_year = df.query(query)
-    min_idx = df_cheapest_year["dollar_price"].idxmin()
+    cheapest_year = df.query(query)
+    min_idx = cheapest_year["dollar_price"].idxmin()
+    # "country_name(country_code): $dollar_price"
     
-    return df_cheapest_year.loc[min_idx]["dollar_price"]
+    return cheapest_year.loc[min_idx]["dollar_price"]
     
 def get_the_most_expensive_big_mac_price_by_year(year):
     query = f"date == '{year}'"
-    df_expensive_year = df.query(query)
-    max_idx = df_expensive_year["dollar_price"].idxmax()
+    expensive_year = df.query(query)
+    max_idx = expensive_year["dollar_price"].idxmax()
     
-    return df_expensive_year.loc[max_idx]['dollar_price']
+    return expensive_year.loc[max_idx]['dollar_price']
 
 if __name__ == "__main__":
     result_a = get_big_mac_price_by_year(2010,"arg")
