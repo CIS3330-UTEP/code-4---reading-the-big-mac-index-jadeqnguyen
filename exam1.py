7518a547379b28158ee96d2fa54b0e0cf17bc50c
jadeqnguyen <jqnguyen@miners.utep.edu>
2024-03-07 12:27:10 -0700

    class

---
+++ exam1.py
@@ -0,0 +1,46 @@
+# distance = "30"
+# new_distance = float(distance)
+# print(new_distance)
+
+# n = 10
+# while 3 < n:
+#   print("Number 3 is the best number")
+#   n -= 1  
+# print("Code execution has finished")
+
+# fruits = ['kiwi', 'apple', 'grape', 'pear']
+# ﻿number_of_fruits = len(fruits)
+# if number_of_fruits == 5
+#   print(f"There are {number_of_fruits} fruit(s)")
+
+#   def get_a_random_fruit():
+#     fruits = ["apple","kiwi","grape","mango"]
+#     random_fruit = random.choice(fruits)
+
+# number = 99
+# if number % 2 == 0:
+#   if number >= 50:
+#     print(f"the number {number} is even and over 50")
+#   else:
+#     print(f"the number {number} is even and lower 50")
+
+# welcome_message = "Hello"
+# name = "Billy"
+# print(welcome_message+" "+name)
+
+# text = """
+# The narrow path had opened suddenly onto the edge of a great black lake. 
+# Perched atop a high mountain on the other side, its windows sparkling in 
+# the starry sky, was a vast castle with many turrets and towers.\n
+# "No more 'n four to a boat!" Hagrid called, pointing to a fleet of 
+# little boats sitting in the water by the shore. Harry and Ron were 
+# followed into their boat by Nevlille and Hermione.\n
+# """
+# parts = text.split(".")
+
+days_of_the_week = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+for day in days_of_the_week:
+  if day == "friday":
+    print("TGIF")
+  else:
+    print("Day not found") 
